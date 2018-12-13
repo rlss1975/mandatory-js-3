14fd439b5263576bdeb50de6d55f1a929a22fc29
Ricardo Soriano <rsorianosanchis@gmail.com>
2018-12-13 21:46:27 +0100

    v.9

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 //
-//main column 1 i main /index.html
+//main column 1 
 let vBreedUL = document.querySelector('#ulBreed');
 //main column 2
 let vSubBreedUL = document.querySelector('#ulSubBreed');
@@ -12,29 +12,32 @@ let vSelectedBreed = document.getElementsByClassName('classBreed');
 let vSelectedSubBreed = document.getElementsByClassName('classSubBreed');
 let selectedItemIListorna;
 let lastSelectedBreed;
-//
 //Internal data i .js
 let vButtonUrl;
 let allBreedList = 'https://dog.ceo/api/breeds/list/all';
 let allBreedAllRandomImages = 'https://dog.ceo/api/breeds/image/random';
 //let breedRandomImage = `https://dog.ceo/api/breed/${}/images/random`;
 //let subBreedRandomImage = `https://dog.ceo/api/breed/${}/${}/images/random`;
+//
+//internal data
 let data;
 let flagBreedSelected = 0;
 let flagSubBreedSelected = 0;
-//
+let reloadBreed;
+let reloadSubBreed;
+let userReload = false;
 //
 /*******************************************/
 /*********PROGRAM EXECUTION CONTROL*********/
 /*******************************************/
+vButton.addEventListener('click',()=>{ajaxGet(vButtonUrl,getImgUrl)});
+//
+//hashKontroll();//Här ska programmet bara komma om reload page.
 //
 ajaxGet(allBreedList,renderaList);
 //
-vButton.addEventListener('click',()=>{ajaxGet(vButtonUrl,getImgUrl)});
-//
 /*******************************************/
 /*******************************************/
-
 //
 function ajaxGet (url,myFunction){
 	let req = new XMLHttpRequest();
@@ -50,35 +53,31 @@ function ajaxGet (url,myFunction){
 	req.addEventListener('error',()=>{console.log('Förfrågan lyckades inte nå serven');});
 	req.send(null);
 };
-
+//
 function renderaList (dataIn){
-	//Vi renderar list på column 1 ( general breed lista)
-	
+	//
+	//Renderar list på column 1 ( general breed lista)
 	if (flagBreedSelected === 0 && flagSubBreedSelected === 0){
-
 		data = JSON.parse(dataIn);
-		//vaciamos lista subrazas
 		for (let breed in data.message){
 			//console.log(breed);
 			let sthetic = capitalize(breed);
 			//console.log(sthetic);
 			vBreedUL.innerHTML += `<li class="classBreed" id="${breed}">${sthetic}</li>`;
 		}
-		//debugger;
 		listAddEvent(vSelectedBreed);
-		//debugger;
 		vButtonUrl ='';
 		vButtonUrl = allBreedAllRandomImages;
-		// vi passar på för att rendera en första img för att göra funktionalitet snyggare.
+		// vi passar på för att rendera default första img. 
 		ajaxGet(vButtonUrl,getImgUrl)
-		//har controlerar vi om någon har clikat nån breed col 1
-		
-	}else	if(flagBreedSelected === 1 && flagSubBreedSelected === 0){
+		//
+		//
+	}else	if(flagBreedSelected === 1 && flagSubBreedSelected === 0){ // <== har controlerar vi om någon har clikat nån breed col 1.
 		//random image for alla breed 
 		vSubBreedUL.innerHTML = '';
+		if (userReload === true){selectedItemIListorna = reloadBreed;};
 		for (let breed of data.message[selectedItemIListorna]) {
-				// bara som memmory for använda senare
-				lastSelectedBreed = selectedItemIListorna;
+				lastSelectedBreed = selectedItemIListorna;// bara som memmory for använda senare
 				//vi passar på att rendera och visar sub-breed list av selected breed
 				console.log(breed);
 				let sthetic = capitalize(breed);
@@ -87,53 +86,45 @@ function renderaList (dataIn){
 		//debugger;
 		listAddEvent(vSelectedSubBreed);
   	vButtonUrl ='';
-		vButtonUrl = `https://dog.ceo/api/breed/${selectedItemIListorna}/images/random`;
-		
+		vButtonUrl = `https://dog.ceo/api/breed/${selectedItemIListorna}/images/random`;	
 		ajaxGet(vButtonUrl,getImgUrl);
 		window.location.hash = `#${selectedItemIListorna}`;
-	
+	//
+	//
 	}else if (flagBreedSelected === 0 && flagSubBreedSelected === 1){	
 		vButtonUrl ='';
 		vButtonUrl = `https://dog.ceo/api/breed/${lastSelectedBreed}/${selectedItemIListorna}/images/random`;
-		
 		ajaxGet(vButtonUrl,getImgUrl);
 		window.location.hash = `#${lastSelectedBreed}/${selectedItemIListorna}`;
 	}
 };
-
-		
-
 //
-/* Här vi lägar till värje item i listor en event för att veta 
+/* Här vi lägar till värje item i listor en event för att veta och
 vilket här clickt usr (breed /eller/ subbreed) */
 function listAddEvent(liElements){
 	console.log(liElements);;
 	//debugger;
-		for (let liBreed of liElements){
-				liBreed.addEventListener('click',(event)=>{
-							//debugger;
-							flagBreedSelected = 0;
-							flagSubBreedSelected = 0;
-							console.log(event);
-							selectedItemIListorna = event.target.id;
-							console.log(selectedItemIListorna);
-							if(event.target.className ==='classBreed'){
-								flagBreedSelected = 1;
-								flagSubBreedSelected = 0;
-								
-							}else if (event.target.className === 'classSubBreed'){
-								flagBreedSelected = 0;
-								flagSubBreedSelected = 1;
-								
-
-							}else{
-								console.log("error i listAddEvent()");
-							}
-							renderaList(data);
-							
-
-				});
-		};
+	for (let liBreed of liElements){
+		liBreed.addEventListener('click',(event)=>{
+			//debugger;
+			flagBreedSelected = 0;
+			flagSubBreedSelected = 0;
+			console.log(event);
+			selectedItemIListorna = event.target.id;
+			console.log(selectedItemIListorna);
+			if(event.target.className ==='classBreed'){
+				flagBreedSelected = 1;
+				flagSubBreedSelected = 0;
+				
+			}else if (event.target.className === 'classSubBreed'){
+				flagBreedSelected = 0;
+				flagSubBreedSelected = 1;
+			}else{
+				console.log("error i listAddEvent()");
+			}
+			renderaList(data);
+		});
+	};
 };
 //
 //capitalize viktor®
@@ -143,31 +134,48 @@ function capitalize(str) {
 //
 //När man klikar på knappen vi gör en req till adressen som är laddad i vButtonUrl. Adressen ska bli kopplad till någon random img iAPI beroende på vilket adressen behover program logik. Detta adressen är laddad i <img>.
 function getImgUrl (imgUrlJson) {
-				console.log(imgUrlJson);
-				let imgUrl = JSON.parse(imgUrlJson);
-				console.log(imgUrl.message);
-				vHundImg.setAttribute('src', imgUrl.message);
-				//
-				//set Hund Name
-				let strUrl = imgUrl.message;
-				let arrUrl = strUrl.split('/');				
-				arrUrl = arrUrl[4].replace('-',' ');
-				let name = capitalize(arrUrl);
-				vHundTitle.textContent = `${name}.`
-				
+	console.log(imgUrlJson);
+	let imgUrl = JSON.parse(imgUrlJson);
+	console.log(imgUrl.message);
+	vHundImg.setAttribute('src', imgUrl.message);
+	//
+	//set Hund Name
+	let strUrl = imgUrl.message;
+	let arrUrl = strUrl.split('/');				
+	arrUrl = arrUrl[4].replace('-',' ');
+	let name = capitalize(arrUrl);
+	vHundTitle.textContent = `${name}.`;
+	//userReload = false;
 };
+//
+function hashKontroll(){
+	//debugger;
+	let vHash = window.location.hash;
+	ajaxGet(allBreedList,renderaList);
+	if (vHash != false){
+		console.log(vHash);
+		//debugger;
+		vHash = vHash.split('/');
+		console.log(vHash);
+		if (vHash.length === 1){
+			flagBreedSelected = 1;
+			flagSubBreedSelected = 0;
+			userReload = true;
 
+		}else if (vHash.length === 2){
+			flagBreedSelected = 0;
+			flagSubBreedSelected = 1;
+			userReload = true;
+		}else{
+			flagBreedSelected = 0;
+			flagSubBreedSelected = 0;
+			userReload = false;
+		}
+		reloadBreed = vHash[0].slice(1);
+		console.log(reloadBreed);
+		reloadSubBreed = vHash[1];
+		console.log(reloadSubBreed);
+	}
+};
 //
 
-
-
-
-
-//
-
-//aqui convertimos en listeners a los elementos 
-
-
-//1. HAY QUE HACER UN EVENTO CLICK SOBRE LA CLASE BREED EN LA LISTA GENERAL. ESTO LLAMARA A LA FUNCION GET AJAX SOBRE LA RAZA EN CONCRETO Y USAREMOS SUS DATOS.
-//2. OTRO EVENTO SOBRE LA CLASE SUBREED QUE HAGA LO MISMO ACONTANDO A DATOS SUBREED  
-// aqui un event listener click sobre breed que active una logica de busqueda sobre objeto para ver sus objetos
